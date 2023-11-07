66f9ee6cd78c1c09e6f8d73b79e71452e6bf302e
JAMSHEDWEBDEV <jamsheddc60@gmail.com>
2023-11-08 00:10:53 +0600

    added footer component

---
+++ src/Pages/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+
+import { Link } from 'react-router-dom';
+import footerLogo from '../../assets/exertion-logo.svg';
+import {BsFacebook,BsTwitter,BsLinkedin,BsBehance} from 'react-icons/bs';
+
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-slate-700 text-base-content">
+            <aside>
+                <img src={footerLogo} alt="footer_image" />
+                <p className='text-orange-100 font-bold'>Exertio Jobsmarket Ltd.<br />Providing reliable tech since 1992</p>
+                <div className='flex gap-6 my-6 text-orange-100 text-3xl'>
+                    <Link className='hover:text-red-400'><BsFacebook/></Link>
+                    <Link className='hover:text-red-400'><BsTwitter/></Link>
+                    <Link className='hover:text-red-400'><BsLinkedin/></Link>
+                    <Link className='hover:text-red-400'><BsBehance/></Link>
+                </div>
+            </aside>
+            <nav>
+                <header className="footer-title text-white">Services</header>
+                <a className="link link-hover text-orange-100 font-bold">Web Development</a>
+                <a className="link link-hover text-orange-100 font-bold">Web Design</a>
+                <a className="link link-hover text-orange-100 font-bold">Digital Marketing</a>
+                <a className="link link-hover text-orange-100 font-bold">Graphic Design</a>
+                <a className="link link-hover text-orange-100 font-bold">Job Post</a>
+            </nav>
+            <nav>
+                <header className="footer-title text-white">Company</header>
+                <a className="link link-hover text-orange-100 font-bold">About us</a>
+                <a className="link link-hover text-orange-100 font-bold">Contact</a>
+                <a className="link link-hover text-orange-100 font-bold">Jobs</a>
+                <a className="link link-hover text-orange-100 font-bold">Press kit</a>
+            </nav>
+            <nav>
+                <header className="footer-title text-white">Legal</header>
+                <a className="link link-hover text-orange-100 font-bold">Terms of use</a>
+                <a className="link link-hover text-orange-100 font-bold">Privacy policy</a>
+                <a className="link link-hover text-orange-100 font-bold">Cookie policy</a>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
